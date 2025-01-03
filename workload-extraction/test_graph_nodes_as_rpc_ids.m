8707697e5135159e7d816bfa4997afe18c207b9e
h3rby7 <herbielutz@gmx.net>
2025-01-03 19:00:48 +0100

    fix looping wrongly over permutations

--- workload-extraction/test_graph_nodes_as_rpc_ids.m
+++ workload-extraction/test_graph_nodes_as_rpc_ids.m
@@ -14,51 +14,101 @@ end
 
 
 function [unique_node_name_options] = node_options(graph, node, rpc_id)
+    % Get all naming options for this node and its successors
+    % As we do not know if the first of n successors would be .1 or .n
     children = graph.successors(node);
     child_count = height(children);
     if child_count == 0
+        % no children -> leaf node
         unique_node_name_options = {rpc_id};
         return
     end
+    % calculate permutations for children
     permutations = perms(1:child_count);
     permutation_count = height(permutations);
-    % holds 1 entry per permutation
-    % entry can have multiple results
+
+    % holds 1 cell as entry per permutation
+    % cell can have multiple results, as children may also have more than
+    % one naming options
     all_node_name_options = cell(permutation_count,1);
     for i=1:permutation_count
-        % should hold 1 entry per child
-        % entry can have multiple results
+
+        % should hold 1 entry cell per child
+        % cell can have multiple results, as children may also have more than
+        % one naming options
         permutation_options = cell(child_count,1);
+
+        % within the permutation we get the potential node names for
+        % all children
         for j=1:child_count
-            id_opt = rpc_id + "." + permutations(j, i);
+            % id_opt -> the suggested rpc_id based on the permutation
+            id_opt = rpc_id + "." + permutations(i, j);
+
+            % get all node-name combination of successors, given we had
+            % used the id_opt as this child's node_name
             child_options = node_options(graph, children(j), id_opt);
             permutation_options{j} = child_options;
         end
+
+        % flatten the nodenames as in the end we want a string array
         flattened = flatten_permutations(permutation_options);
+
+        % TODO: flattened may return more than one result, we need to
+        % seperately prepend rpc_id to each entry and add them to the list
+
+        % add result to potential list
         all_node_name_options{i} = [rpc_id sort(flattened{:})];
     end
+
+    % the list may contain duplicate names, we filter them out to prevent
+    % double computations when passing the result back up.
     [~, unique_idx] = unique(cell2table(all_node_name_options));
     unique_node_name_options = all_node_name_options(unique_idx,:);
 end
 
 
 function [output] = flatten_permutations(permutation_options)
+    % permutation_options contains a cell of varying size for each child
+    % within each cell combinations of potential node_names reside
+    % we need to combine all entries with all other entries to get all
+    % possible combinations across the children entries
+    % Approach: 'mulitply' them one-by-one
+
+    % take the first entry as base
     flattened = permutation_options{1};
+
+    % start the loop with the second item
     for i=2:height(permutation_options)
+
+        % options until now
         current_size = width(flattened);
+
+        % options presented by the next item to multiply with
         next_el_size = width(permutation_options(i));
+
+        % create an intermediate result cell
         tmp_i = cell(1, current_size);
+
         for o=1:current_size
+
+            % item from the already combined options
             option = flattened{o};
+
+            % create an intermediate result to hold the results of this one
+            % item and the items of 'next'
             tmp_o = cell(1, next_el_size);
+
             for n=1:next_el_size
+                % combine the next item and the already combined option
                 next = permutation_options{i}{n};
-                tmp_o{n} = [option next];
+                tmp_o{n} = [option{:} next];
             end
             tmp_i{o} = tmp_o;
         end
-        % must horzcat or something
         flattened = tmp_i;
     end
     output = flattened{1};
+    if class(output) == "string"
+        output = {output};
+    end
 end
