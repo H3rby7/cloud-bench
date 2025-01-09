3f32e1b67ae610c72565e0a3dbdca5dc947daec0
h3rby7 <herbielutz@gmx.net>
2025-01-09 14:54:44 +0100

    fix locations after structural DIR changes

--- workload-extraction/sample_services/config.m
+++ workload-extraction/sample_services/config.m
@@ -3,14 +3,14 @@ function [trace_location, trace_header_lines, output_dir_sequential, output_dir_
 
     % *************** Trace options ***************
     % trace_location of alibaba cluster trace microservices v2022
-    trace_location = "../traces/alibaba/2022-ms/CallGraph/CallGraph_0.csv";
+    trace_location = "../../traces/alibaba/2022-ms/CallGraph/CallGraph_0.csv";
     % how many lines to skip
     trace_header_lines = 1;
 
 
     % *************** Directories for output ***************
     % Root DIR for output
-    output_dir_root = "../traces-mbench";
+    output_dir_root = "../../traces-mbench";
     % DIR within root DIR to hold sequential output
     output_dir_sequential = output_dir_root+"/sequential";
     % DIR within root DIR to hold parallel output
