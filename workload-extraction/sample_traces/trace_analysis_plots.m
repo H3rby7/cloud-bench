04e3d043491a922920103cae57d3de018509ea64
h3rby7 <herbielutz@gmx.net>
2025-01-17 13:49:51 +0100

    trace_analysis_plots add support for cluster selection as input; ensure formatting being applied for last plot

--- workload-extraction/sample_traces/trace_analysis_plots.m
+++ workload-extraction/sample_traces/trace_analysis_plots.m
@@ -2,11 +2,12 @@ function [] = trace_analysis_plots(input_data, ylimits)
 
     % ************** Split graphs by cluster **************
     trace_count_by_group_by_minutes = groupcounts(input_data,["cluster","ts_minute"]);
-    cluster_count = max(trace_count_by_group_by_minutes.cluster);
+    clusters = unique(input_data.cluster);
+    cluster_count = height(clusters);
     
     % Calculate grid sizes
-    plot_count = 8;
-    cluster_per_figure = ceil(cluster_count / plot_count);
+    max_cluster_per_figure = 5;
+    plot_count = ceil(cluster_count / max_cluster_per_figure);
     subplot_cols = ceil(sqrt(plot_count));
     subplot_rows = ceil(plot_count / subplot_cols);
     
@@ -16,8 +17,7 @@ function [] = trace_analysis_plots(input_data, ylimits)
     f = figure();
     set(f,'Visible','off');
     for c=1:cluster_count
-    
-        if mod(c, cluster_per_figure) == 1
+        if mod(c, max_cluster_per_figure) == 1
             % Start a new sub_plot
             active_subplot_idx = active_subplot_idx + 1;
             active_plot = subplot(subplot_rows, subplot_cols, active_subplot_idx);
@@ -25,13 +25,13 @@ function [] = trace_analysis_plots(input_data, ylimits)
         end
     
         % Plot into active sub_plot
-        data = trace_count_by_group_by_minutes(trace_count_by_group_by_minutes.cluster == c, ["ts_minute", "GroupCount"]);
+        data = trace_count_by_group_by_minutes(trace_count_by_group_by_minutes.cluster == clusters(c), ["ts_minute", "GroupCount"]);
         label = "C" + num2str(c, "%02i");
         plot(active_plot, data, "ts_minute", "GroupCount", "DisplayName", label);
-        ylim(active_plot, ylimits);
         
-        if mod(c, cluster_per_figure) == 0
+        if mod(c, max_cluster_per_figure) == 0 || c == cluster_count
             % Finishing touches to active sub_plot
+            ylim(active_plot, ylimits);
             hold(active_plot, 'off');
             yscale(active_plot, "log");
             ylabel(active_plot, "Trace Count");
