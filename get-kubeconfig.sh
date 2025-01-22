ec86c54369d885edf157ae7eaca9f2f7da9009f0
h3rby7 <herbielutz@gmx.net>
2025-01-22 13:17:03 +0100

    fix get-kubeconfig script

--- get-kubeconfig.sh
+++ get-kubeconfig.sh
@@ -2,15 +2,23 @@
 
 # username="USER INPUT"
 read -p "Enter username: " username
-# sshkeyloc="USER INPUT"
-read -p "Enter location of your ssh key: " sshkeyloc
 # fqdn="USER INPUT"
 read -p "Enter FQDN of node-0: (e.g. apt001.apt.emulab.net)" fqdn
+# sshkeyloc="USER INPUT"
+read -p "Enter location of your ssh key: " sshkeyloc
 
 echo "Getting kubeconfig from ${username}@${fqdn}:/users/${username}/.kube/config"
 
 scp -i ${sshkeyloc} ${username}@${fqdn}:/users/${username}/.kube/config config.yaml
 
-cat config.yaml | sed -r "s/server:.*/server: https://${fqdn}:6443/" > cloudlab_kubeconfig.yaml
+echo "Retrieved kubeconfig, adjusting server URL..."
+
+cat config.yaml | sed -r "s?server:.*?server: https://${fqdn}:6443?" > cloudlab_kubeconfig.yaml
 
 rm config.yaml
+
+echo "****************** cloudlab_kubeconfig.yaml ******************"
+echo ""
+cat cloudlab_kubeconfig.yaml
+echo ""
+echo "****************** cloudlab_kubeconfig.yaml ******************"
