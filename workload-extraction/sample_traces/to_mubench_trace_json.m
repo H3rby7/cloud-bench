c4dc2a461fe600bf17970c07de5baf7655ee1505
h3rby7 <herbielutz@gmx.net>
2025-01-23 15:37:43 +0100

    fix creating InternalServices CM from old incompatible code

--- workload-extraction/sample_traces/to_mubench_trace_json.m
+++ workload-extraction/sample_traces/to_mubench_trace_json.m
@@ -1,6 +1,9 @@
 function [output] = to_mubench_trace_json(input)
     % Remove "USER" from json
     % Make names k8s-conformous
+
+    % TODO: using struct conversion the [{"svc":[{}]}] bracket construct
+    % breaks, need to work with string magic instead!
     output = cell(height(input),1);
     for i=1:height(input)
         as_struct = jsondecode(input{i});
