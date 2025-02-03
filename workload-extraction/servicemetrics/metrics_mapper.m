39f425dad942582884e9a180a9f37b4970dedbb9
h3rby7 <herbielutz@gmx.net>
2025-02-03 15:32:15 +0100

    Map-Reduce to mean, variance, skewness, kurtosis

--- workload-extraction/servicemetrics/metrics_mapper.m
+++ workload-extraction/servicemetrics/metrics_mapper.m
@@ -6,6 +6,28 @@ function metrics_mapper(data, ~, intermKVStore, max_timestamp)
         return;
     end
 
-    add(intermKVStore, "entries", relevant_entries);
+    groups = relevant_entries.ms_name;
+    
+    % Find the unique groups
+    [ms_name,~,idx] = unique(groups, 'stable');
+    
+    % Group delays by idx and apply @grpstatsfun function to each group
+    cpu_utilization = accumarray(idx,relevant_entries.cpu_utilization,size(ms_name),@grpstatsfun);
+    memory_utilization = accumarray(idx,relevant_entries.memory_utilization,size(ms_name),@grpstatsfun);
+    % Combine to AxB double
+    combined = [vertcat(cpu_utilization{:}) vertcat(memory_utilization{:})];
+    % Convert AxB double to a cell array with 
+    % A rows of size 1 containing B doubles
+    as_cell = mat2cell(combined, ones(height(combined), 1), width(combined));
+    addmulti(intermKVStore,ms_name,as_cell);
 
+end
+
+function out = grpstatsfun(x)
+    n = length(x); % count
+    m = sum(x)/n; % mean
+    v = sum((x-m).^2)/n; % variance
+    s = sum((x-m).^3)/n; % skewness without normalization
+    k = sum((x-m).^4)/n; % kurtosis without normalization
+    out = {[n, m, v, s, k]};
 end
--- workload-extraction/servicemetrics/metrics_reducer.m
+++ workload-extraction/servicemetrics/metrics_reducer.m
@@ -1,11 +1,48 @@
-function metrics_reducer(key, intermValIter, outKVStore)
-    T = table();
-
-    % Append all entries we have to the table
+function metrics_reducer(ms_name, intermValIter, outKVStore)
+    ms_name = string(ms_name);
+    % Accumulate results
+    cpu_n = [];
+    cpu_m = [];
+    cpu_v = [];
+    cpu_s = [];
+    cpu_k = [];
+    memory_n = [];
+    memory_m = [];
+    memory_v = [];
+    memory_s = [];
+    memory_k = [];
     while hasnext(intermValIter)
-        t_metrics = getnext(intermValIter);
-        T = [T;t_metrics];
+        value = getnext(intermValIter);
+        cpu_n = [cpu_n; value(1)];
+        cpu_m = [cpu_m; value(2)];
+        cpu_v = [cpu_v; value(3)];
+        cpu_s = [cpu_s; value(4)];
+        cpu_k = [cpu_k; value(5)];
+        memory_n = [memory_n; value(6)];
+        memory_m = [memory_m; value(7)];
+        memory_v = [memory_v; value(8)];
+        memory_s = [memory_s; value(9)];
+        memory_k = [memory_k; value(10)];
     end
 
-    add(outKVStore, key, T);
+    cpu_stats = combinestatsfun("cpu", cpu_n, cpu_m, cpu_v, cpu_s, cpu_k);
+    memory_stats = combinestatsfun("memory", memory_n, memory_m, memory_v, memory_s, memory_k);
+    
+    outValue = [table(ms_name) cpu_stats memory_stats];
+
+    add(outKVStore,ms_name,outValue);
+end
+
+function out = combinestatsfun(prefix, n, m, v, s, k)
+    % Combine the intermediate results
+    count = sum(n);
+    meanVal = sum(n.*m)/count;
+    d = m - meanVal;
+    variance = (sum(n.*v) + sum(n.*d.^2))/count;
+    skewnessVal = (sum(n.*s) + sum(n.*d.*(3*v + d.^2)))./(count*variance^(1.5));
+    kurtosisVal = (sum(n.*k) + sum(n.*d.*(4*s + 6.*v.*d +d.^3)))./(count*variance^2);
+    
+    out = table(count, meanVal, variance, skewnessVal, kurtosisVal);
+    out.Properties.VariableNames = [prefix+"_count", prefix+"_mean", prefix+"_variance", prefix+"_skewness", prefix+"_kurtosis"];
+
 end
